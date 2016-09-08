0e5b8160f24e931c3dae62cfb554d9bc23d3dd96
Vishal Mishra <vishal.mishra@dubizzl.com>
2016-09-08 16:46:19 +0400

    Added cucumber seleniumt test initial draft

---
+++ src/main/java/com/amazon/basepage/CucumberReport.java
@@ -0,0 +1,31 @@
+package com.amazon.basepage;
+import net.masterthought.cucumber.ReportBuilder;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by vishal on 9/8/16.
+ */
+
+public class CucumberReport {
+
+    private static final String jsonPath = "/target/cucumber.json";
+    private static final String reportDirectory = "/target/cucumber-html-reports";
+    private static final String userDirector = System.getProperty("user.dir");
+    public static void main(String[] args) throws Exception {
+        GenerateReport();
+    }
+
+    private static void GenerateReport() throws Exception {
+        File reportOutputDirectory = new File(userDirector + reportDirectory);
+        List<String> jsonReportFiles = new ArrayList<String>();
+        jsonReportFiles.add(userDirector + jsonPath);
+        ReportBuilder reportBuilder = new ReportBuilder(jsonReportFiles,
+                reportOutputDirectory, "",
+                "UI Tests execution report of Common Admin Application",
+                "cucumber-jvm", true, false, true, true, false, "", true);
+        reportBuilder.generateReports();
+    }
+}
---
+++ src/test/resources/FunctionalTests/Features/AmazonProductSearch.feature
@@ -0,0 +1,15 @@
+Feature: Amazon Product Search
+  As a user
+  I want search product
+  On amazon shopping website
+
+
+  @RegressionTests @SmokeTests
+  Scenario: Search for information technology book
+    Given I am on Amazon homepage
+    When I look for information technology books
+    And I search for The Cucumber Book by Matt Wynne and Aslak Hellesoy
+    Then I should the book title The Cucumber Book: Behaviour-Driven Development for Testers and Developers (Pragmatic Programmers)
+    And author by by Matt Wynne  (Author), Aslak Hellesoy (Author)
+    And breadcrumbs should show Books › Computers & Technology › Programming
+
